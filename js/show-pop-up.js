2cccc86351b4170f1c6b1ce2a1709fce33dbef99
weasel <sendemailtoromar@gmail.com>
2023-08-31 00:49:04 +0500

    Добавлены анимации для раскрывающегося главного меню. На панель главного меню добавлена форма «Оставить заявку». Добавлен pop-up «Заказать звонок». Добавлена анимированная шторка открывающаяся при открытии главного меню и pop-up. Прочие исправления.

---
+++ js/show-pop-up.js
@@ -0,0 +1,39 @@
+function openPopUp() {
+    let popUpArea = document.querySelector(".pop-up-area");
+    let popUp = document.querySelector(".pop-up");
+
+    popUpArea.style.display = "flex"
+    openCurtain()
+
+    setTimeout(() => {
+        popUp.style.transform = "translateY(0)"
+        popUp.style.opacity = 1
+    }, 1000)
+}
+
+
+function closePopUp() {
+    let popUpArea = document.querySelector(".pop-up-area");
+    let popUp = document.querySelector(".pop-up");
+
+    popUp.style.transform = "translateY(1em)"
+    popUp.style.opacity = 0
+
+    setTimeout(() => {
+        closeCurtain()
+    }, 600)
+
+    setTimeout(() => {
+        popUpArea.style.display = "none"
+    }, 1000)
+
+    
+}
+
+
+document.querySelector(".call-me-pretty").onclick = openPopUp;
+document.querySelector(".pop-up-area").onclick = closePopUp;
+document.querySelector(".pop-up-back").onclick = closePopUp;
+document.querySelector(".pop-up").onclick = function (event) {
+	event.stopPropagation(); //блокируется дальнейшая передача события
+};
--- js/show_menu.js
+++ js/show_menu.js
@@ -1,19 +1,260 @@
 
-function toogleMenu() {
-    let menu = document.querySelector(".expanded-header")
+let isOpenMenu = false
+let isOpenSubmissionForm = false
 
-    if (menu.classList.contains("expanded-header-show")){
-        menu.classList.remove("expanded-header-show")
-    } else {
-        menu.classList.add("expanded-header-show")
-    }
+function openCurtain(){
+    // задергивает шторку на экран
+
+    let curtain_1 = document.createElement('div');
+    curtain_1.className = "curtain-1";
+    document.body.append(curtain_1);
+
+    let curtain_2 = document.createElement('div');
+    curtain_2.className = "curtain-2";
+    document.body.append(curtain_2);
+
+    let curtain_3 = document.createElement('div');
+    curtain_3.className = "curtain-3";
+    document.body.append(curtain_3);
+
+    let curtain_main = document.createElement('div');
+    curtain_main.className = "curtain-main";
+    curtain_main.onclick = toogleMenu;
+    document.body.append(curtain_main);
+
+    const cwa = document.body.clientWidth;
+    document.body.style.overflow = "hidden";
+    const cwl = document.body.clientWidth;
+
+    widthScroll = cwl - cwa
+
+    if ( cwa < cwl ) {
+        document.body.style.paddingRight = cwl - cwa + "px";
+    };
+
+    let btn = document.querySelector(".call-me-pretty");
+    current_width = Number(getComputedStyle(btn).right.replace("px", ""))
+    btn.style.right =  current_width + cwl - cwa + "px";
 
 }
 
+function closeCurtain(){
+    // убирает шторку с экрана
+
+    let curtain_1 = document.querySelector(".curtain-1");
+    let curtain_2 = document.querySelector(".curtain-2");
+    let curtain_3 = document.querySelector(".curtain-3");
+    let curtain_main = document.querySelector(".curtain-main");
+
+    curtain_1.style.transform = "translateX(200vw)";
+    curtain_2.style.transform = "translateX(200vw)";
+    curtain_3.style.transform = "translateX(200vw)";
+    curtain_main.style.transform = "translateX(200vw)";
+
+    setTimeout(() => {
+        curtain_1.remove();
+        curtain_2.remove();
+        curtain_3.remove();
+        curtain_main.remove();
+    }, 2000);
+
+    document.body.style.overflow = "";
+    document.body.style.paddingRight = "";
+
+    let btn = document.querySelector(".call-me-pretty");
+    btn.style.right=""
+}
+
+function animShowPoints (points) {
+    // анимирует пункты меню во время раскрытия главного меню
+    let delay = 1;
+
+    points.forEach((item, index, arr) => {
+        if (item !== null) {
+            setTimeout(() => {
+                item.style.transform = "translateY(1em)";
+                item.style.transform = "translateY(0)";
+                item.style.opacity = 0;
+                item.style.opacity = 1;
+
+            }, delay);
+
+            delay += 100;
+        }
+    })
+}
+
+function animHidePoints (points) {
+    // анимирует пункты меню во время закрытия главного меню
+    let delay = 0;
+
+    points.reverse()
+    points.forEach((item, index, arr) => {
+        if (item !== null) {
+            setTimeout(() => {
+                item.style.transform = "translateY(0)";
+                item.style.transform = "translateY(1em)";
+                item.style.opacity = 1;
+                item.style.opacity = 0;
+            }, delay);
+
+            delay += 50;
+        }
+    })
+}
+
+function toogleMenu() {
+    // раскрывает и скрывает главное меню сайта
+    let menu = document.querySelector(".expanded-header");
+
+    let menu_points = [
+        document.querySelector(".expanded-header-caption-big"),
+        document.querySelector(".expanded-header-caption-little"),
+        ...document.querySelector(".expanded-header-nav").children,
+        document.querySelector(".expanded-header-submit"),
+        ...document.querySelectorAll(".expanded-header-contacts-point"),
+        ...document.querySelectorAll(".expanded-header-line"),
+    ]
 
 
-let btns = document.querySelectorAll(".btn-connect-show-menu")
+    if (menu.classList.contains("expanded-header-show")){
+        // ЗАКРЫТИЕ ПАНЕЛИ САЙТА
 
-for (var i in btns){
-	btns[i].onclick = toogleMenu;
-};
+        isOpenMenu = false
+
+        animHidePoints(menu_points)
+
+        setTimeout(() => {
+            menu.classList.remove("expanded-header-show");
+            closeCurtain();
+        }, 1000)
+        
+        // ↓ если во время закрытия открыта форма отправки запроса
+        if (isOpenSubmissionForm) {
+            let nav = document.querySelector(".expanded-header-nav");
+            let submit = document.querySelector(".expanded-header-submit");
+            let form = document.querySelector(".expanded-header-form");
+            
+
+            form.style.transform = "translateY(1em)";
+            form.style.opacity = 0;
+
+            setTimeout(() => {
+                form.style.transform = "";
+                form.style.opacity = "";
+                nav.style.display = "flex";
+                submit.style.display = "flex";
+                form.style.display = "none";
+                nav.style.transform = "";
+                nav.style.opacity = "";
+                submit.style.transform = "";
+                submit.style.opacity = "";
+
+                isOpenSubmissionForm = false
+                
+            }, 1500)
+        }
+        // let stretcher = document.querySelector(".expanded-header-stretcher");
+        // stretcher.style.maxHeight = "0px"
+        
+    } else {
+        // ОТКРЫТИЕ ПАНЕЛИ САЙТА
+
+        isOpenMenu = true
+        
+        openCurtain();
+
+        setTimeout(() => {
+            menu.classList.add("expanded-header-show");
+        }, 500)
+
+        setTimeout(() => {
+            animShowPoints(menu_points);
+        }, 1000)
+        
+    }
+}
+
+
+function openSubmissionForm (isOnWindow) {
+    // открывает форму отправки заявки внутри уже открытого header
+
+    isOpenSubmissionForm = true
+
+    let nav = document.querySelector(".expanded-header-nav");
+    let submit = document.querySelector(".expanded-header-submit");
+    let form = document.querySelector(".expanded-header-form");
+    let back = document.querySelector(".expanded-header-form-back");
+    let stretcher = document.querySelector(".expanded-header-stretcher");
+    stretcher.style.maxHeight = getComputedStyle(stretcher).height;
+
+    function returnToMenu() {
+        form.style.transform = "";
+        form.style.opacity = "";
+
+        setTimeout(() => {
+            nav.style.display = "flex";
+            submit.style.display = "flex";
+        }, 600)
+
+        setTimeout(() => {
+            nav.style.display = "flex";
+            submit.style.display = "flex";
+            form.style.display = "none";
+            nav.style.transform = "translateX(0)";
+            nav.style.opacity = 1;
+            submit.style.transform = "translateX(0)";
+            submit.style.opacity = 1;
+            stretcher.style.maxHeight = ""
+            isOpenSubmissionForm = false
+        }, 650)
+    }
+
+
+    if (isOpenMenu) {
+        // ЕСЛИ ПАНЕЛЬ ОТКРЫТА
+        nav.style.transform = "translateX(-2em)";
+        nav.style.opacity = 0;
+        submit.style.transform = "translateX(-2em)";
+        submit.style.opacity = 0;
+
+        form.style.display = "flex";
+
+        setTimeout(() => {
+            nav.style.display = "none";
+            submit.style.display = "none";
+            form.style.transform = "translateX(0)";
+            form.style.opacity = 1;
+        }, 600)
+
+        back.onclick = returnToMenu
+
+    } else {
+        // ЕСЛИ ПАНЕЛЬ ЕЩЕ НЕ ОТКРЫТА
+        nav.style.display = "none";
+        submit.style.display = "none";
+        form.style.display = "flex";
+        form.style.transform = "translateY(1em)";
+        form.style.opacity = 0;
+
+        console.log("ok")
+
+        setTimeout(() => {
+            form.style.transform = "translateY(0)";
+            form.style.opacity = 1;
+        }, 1500)
+
+        toogleMenu()
+
+        back.onclick = toogleMenu
+    }
+}
+
+
+document.querySelectorAll(".btn-connect-show-menu").forEach(btn => {
+    btn.onclick = toogleMenu;
+})
+
+document.querySelector(".expanded-header-submit").onclick = openSubmissionForm;
+document.querySelector(".menu-bottom-btn-submit").onclick = openSubmissionForm;
+
