b8084db7b15a8992b5f2fb253daf08927fa19a20
weasel <sendemailtoromar@gmail.com>
2023-09-03 00:03:56 +0500

    Добавлены все pop-up

--- js/show-pop-up.js
+++ js/show-pop-up.js
@@ -1,9 +1,49 @@
-function openPopUp() {
+function openPopUp(typePopUp) {
     let popUpArea = document.querySelector(".pop-up-area");
     let popUp = document.querySelector(".pop-up");
 
     popUpArea.style.display = "flex"
     openCurtain()
+
+    const callMeInputs = ["name", "number", "email", "time", "comment", "title", "btns"]
+    const quoteInputs = ["name", "number", "email", "comment", "title", "btns"]
+    const viewInputs = ["name", "number", "email", "time", "comment", "title", "btns"]
+    const sendInputs = ["email", "title", "btns"]
+    const offerInputs = ["name", "number", "email", "price", "comment", "title", "btns"]
+
+    let currentInputs = [];
+    let textTitle = ""
+
+    if (typePopUp === "call-me"){
+        currentInputs = callMeInputs;
+        textTitle = "Заказать звонок"
+    } else if (typePopUp === "request-quote"){
+        currentInputs = quoteInputs;
+        textTitle = "Запросить цену"
+    } else if (typePopUp === "request-view"){
+        currentInputs = viewInputs;
+        textTitle = "Заказать просмотр"
+    } else if (typePopUp === "request-send"){
+        currentInputs = sendInputs;
+        textTitle = "Отправить по почте"
+    } else if (typePopUp === "request-offer"){
+        currentInputs = offerInputs;
+        textTitle = "Предложить свою цену"
+    }
+
+    document.querySelectorAll(".pop-up-area > .pop-up > *").forEach( elem => {
+        elem.style.display = "none"
+
+        const typeElem = elem.getAttribute("type-line")
+
+        if (currentInputs.includes(typeElem)){
+            elem.style.display = "flex"
+
+            if (typeElem === "title"){
+                elem.innerHTML = textTitle
+            }
+        }
+    })
 
     setTimeout(() => {
         popUp.style.transform = "translateY(0)"
@@ -27,9 +67,30 @@ function closePopUp() {
     }, 1000)
 }
 
-document.querySelector(".call-me-pretty").onclick = openPopUp;
+document.querySelector(".call-me-pretty").onclick = () => openPopUp("call-me");
 document.querySelector(".pop-up-area").onclick = closePopUp;
 document.querySelector(".pop-up-back").onclick = closePopUp;
 document.querySelector(".pop-up").onclick = function (event) {
 	event.stopPropagation(); //блокируется дальнейшая передача события
 };
+
+
+const requestQuote = document.querySelector(".page-tender-widghet-price > .btn-submit-light");
+if (requestQuote !== null) {
+    requestQuote.onclick = () => openPopUp("request-quote");
+}
+
+const requestView = document.querySelector(".page-tender-requests > .page-tender-request-view");
+if (requestView !== null) {
+    requestView.onclick = () => openPopUp("request-view");
+}
+
+const requestSend = document.querySelector(".page-tender-requests > .page-tender-request-send");
+if (requestSend !== null) {
+    requestSend.onclick = () => openPopUp("request-send");
+}
+
+const requestOffer = document.querySelector(".page-tender-requests > .page-tender-request-offer");
+if (requestOffer !== null) {
+    requestOffer.onclick = () => openPopUp("request-offer");
+}
