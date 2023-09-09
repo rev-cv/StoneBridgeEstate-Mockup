11488d1f819f6135d5875fb03a485baf7030fc7d
weasel <sendemailtoromar@gmail.com>
2023-09-09 23:23:43 +0500

    анимация логотипа в меню сделана на js в надежде что это будет работать на айпфонъ

--- js/show_menu.js
+++ js/show_menu.js
@@ -265,3 +265,25 @@ if (ctb != null) {
     ctb.onclick = openSubmissionForm;
 }
 
+
+
+const catsleLogo = document.querySelector(".expanded-header > .castle")
+catsleLogo.insertAdjacentHTML('afterbegin', '<div class="castle-curtain"></div>')
+const catsleCurtain = catsleLogo.querySelector(".castle-curtain")
+
+const durCastle = 3400;
+function strtAnimCastle() {
+    catsleCurtain.style.transition = ""
+    catsleCurtain.style.transform = "scaleX(1)"
+
+    setTimeout(() => {
+        catsleCurtain.style.transition = `transform ${durCastle}ms linear`;
+        catsleCurtain.style.transform = "scaleX(0)"
+    }, 100)
+
+    setTimeout(() => {
+        strtAnimCastle()
+    }, durCastle + 100)
+}
+
+strtAnimCastle()
